6280117620f8ea86fa20f03c4a8f16ef348f7873
Romlyp <102391701+Romlyp@users.noreply.github.com>
2022-12-16 18:42:26 +0200

    Add files via upload

---
+++ lessons_js_22/js/script.js
@@ -0,0 +1,29 @@
+// "use strict"
+"use strict"
+//1
+console.log(`Я вивчаю JS`);
+console.log(`JS`);
+
+//2
+let userEyeColor = `#20498`;
+let userName = `Василь`;
+let user;
+console.log(userEyeColor);
+user = userName;
+console.log(user);
+
+
+let colorFon = 30;
+console.log(colorFon);
+colorFon = Number(colorFon);
+console.log(typeof colorFon);
+
+
+if (true) {
+    var size = 15;
+}
+console.log(size);
+
+let koLo = "rom";
+let roBe = `aghbrrvr ${koLo}`;
+console.log(roBe);
